939ce5720c1f0fd7c2f198730a8786cd8f3d9296
Joseph-Pilli <jjoy51517@gmail.com>
2025-07-25 17:41:05 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,10 +16,10 @@ const Header = () => {
   const navItems = [
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
-    { name: 'Skills', href: '#skills' },
-    { name: 'Projects', href: '#projects' },
     { name: 'Education', href: '#education' },
-    { name: 'Contact', href: '#contact' }
+    { name: 'Projects', href: '#projects' },
+    { name: 'Skills', href: '#skills' },
+    { name: 'Contact', href: '#contact' },
   ];
 
   return (
